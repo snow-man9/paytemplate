d8371932bb92a3978586a91bf5cffab10e13f806
hc <1298763465@qq.com>
2023-02-02 18:46:39 +0800

    项目初始化完成

---
+++ src/main/java/com/example/paytemplate/PaytemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.paytemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaytemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paytemplate/PaytemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paytemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
